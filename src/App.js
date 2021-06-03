53b6899c5432839f28411ffc708201a0f1199df0
Salman Malik <salmanmalik@Salmans-MBP.attlocal.net>
2021-06-02 21:05:21 -0500

    created new repo for solution code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+
 import './App.css';
 
+import Home from './components/Home'
+import About from './components/About'
+import Packages from './components/Packages'
+
+
 function App() {
+  
+  const packages = ['Activate your Crystals', 'Monkey Meditation', 'Soak in the Hotsprings', 'Hypnotherapy', 'Mineral Bath']
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <header>
+          <h1 className="title">Welcome to Monty's Mineral SPA</h1>
+
+          <div className="navBar">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About Us</Link>
+              </li>
+              <li>
+                <Link to="/packages">Our Packages</Link>
+              </li>
+            </ul>
+          </div>
+        </header>
+
+        <div className="display">
+          <Route path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/packages" render={() => <Packages packages={packages}/>}  />
+        </div>
+
+      </Router>
+
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+
+
+export default function Home () {
+    return(
+        <div className="homepage">
+            <h1>Visit our resort and change your life!</h1>
+
+            <h1>Only available in MapleWood, Texas</h1>
+            
+            <h1>Check out our wide array of packages and offers!</h1>
+
+        </div>
+    )
+
+}
