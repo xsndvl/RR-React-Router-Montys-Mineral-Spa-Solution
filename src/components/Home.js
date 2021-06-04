23591e9aba2e7bb94e2e75b630050b891bd8e1a4
Salman Malik <salmanmalik@Salmans-MBP.attlocal.net>
2021-06-04 14:38:39 -0500

    solution setup

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ export default function Home () {
         <div className="homepage">
             <h1>Visit our resort and change your life!</h1>
 
-            <h1>Only available in MapleWood, Texas</h1>
+            <h1>Grand opening in StressVille, Texas</h1>
             
             <h1>Check out our wide array of packages and offers!</h1>
 
