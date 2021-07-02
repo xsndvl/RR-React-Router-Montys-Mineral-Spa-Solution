75e59049e0b02950764d47cb9c2495c43bc0e634
Salman Malik <salmanmalik@Salmans-MBP.attlocal.net>
2021-07-02 14:09:52 -0500

    solution branch flushed out

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import Home from './components/Home'
 import About from './components/About'
 import Packages from './components/Packages'
 
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
 
 function App() {
-  
+
   const packages = ['Activate your Crystals', 'Monkey Meditation', 'Soak in the Hotsprings', 'Hypnotherapy', 'Mineral Bath']
 
 
@@ -18,7 +20,7 @@ function App() {
         <header>
           <h1 className="title">Welcome to Monty's Mineral SPA</h1>
 
-          <div className="navBar">
+          {/* <div className="navBar">
             <ul>
               <li>
                 <Link to="/">Home</Link>
@@ -30,13 +32,26 @@ function App() {
                 <Link to="/packages">Our Packages</Link>
               </li>
             </ul>
-          </div>
+          </div> */}
+          <Container>
+            <Nav defaultActiveKey="/" variant="tabs" fill>
+              <Nav.Item>
+                <Nav.Link href="/"> <Link to="/">Home</Link> </Nav.Link>
+              </Nav.Item>
+              <Nav.Item >
+                <Nav.Link eventKey={"aboutPage"}> <Link to="/about">About Us</Link> </Nav.Link>
+              </Nav.Item>
+              <Nav.Item >
+                <Nav.Link eventKey={"packagesPage"}> <Link to="/packages">Our Packages</Link> </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Container>
         </header>
 
         <div className="display">
           <Route path="/" component={Home} />
           <Route path="/about" component={About} />
-          <Route path="/packages" render={() => <Packages packages={packages}/>}  />
+          <Route path="/packages" render={() => <Packages packages={packages} />} />
         </div>
 
       </Router>
